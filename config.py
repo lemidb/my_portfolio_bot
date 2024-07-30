659dddd098572c9d56b64d1a973ec4390a64318f
Lemi Melkamu Shiferaw <LemiM@ethiopianairlines.com>
2024-07-30 17:08:26 +0300

    my new branch commit

--- config.py
+++ config.py
@@ -3,30 +3,59 @@
 bot_token = "7378302156:AAFzV0Qokhwol1i0fTV7umbaKb9sqr7qqmQ"
 my_chat_id = '747116225'
 ProjectInfo = {
-    "jit_library":{"description":"JIT LIBRARY description","technologies":["🐍 python","🖼️ opencv","🖥️ pyqt5","🗃️ mysql"],"outcome":"librarians can now easily manager the library","demo":"jit_library_demo"},
-    "chat_bot":{"description":"chat_bot description ","technologies":["🐍 python","📈 SVM","⚛️ React","🍶 Flask","🗃️ mysql"],"outcome":"users can now get specific information they are looking for without complex navigation","demo":"chatbot_demo"},
-    "proactive":{"description":"proactive description","technologies":["🐍 python","🐳 docker","🕵️‍♂️ Django","📊 matplotlib"],"outcome":"it removed burden from system admins","demo":"proactive_demo"}}
+    "jit_library":{"description":"JITLibTracker is an innovative solution designed to enhance the management and accessibility of library resources within educational institutions. Leveraging advanced computer vision techniques and real-time data processing, JITLibTracker offers librarians an efficient tool for monitoring daily, weekly, and monthly library usage patterns. Simultaneously, it empowers students with immediate access to library occupancy information directly from their dormitories via an integrated Telegram chatbot, promoting informed decision-making and optimal resource utilization.",
+                   "technologies":["🐍 python","🖼️ opencv","🤖 python-telegram-bot","🖥️ pyqt5","🗃️ mysql"],
+                   "outcome":"JITLibTracker significantly enhances the operational efficiency of libraries by automating attendance tracking and providing actionable insights into usage patterns. For students, it offers unparalleled convenience by making library occupancy information readily accessible, encouraging better study space utilization and reducing unnecessary trips. This project exemplifies the potential of technology in transforming traditional library management practices, fostering a more connected and informed academic community",
+                   "demo":"jit_library_demo"},
+    
+    "chat_bot":{"description":"Chatbot System for Organization Websites introduces an AI-driven chatbot designed to enhance user interaction with organizational sites. By utilizing Natural Language Processing (NLP) and Machine Learning (ML), the chatbot simplifies data retrieval, offering real-time access to events, news, and other critical information. Seamlessly integrated with the website and equipped with scraping capabilities, it delivers accurate, summarized data. Social media integration, specifically through Telegram, further expands its reach, making information easily accessible across platforms.",
+                "technologies":["🐍 python","📈 SVM","🤖 python-telegram-bot","⚛️ React","🍶 Flask","🗃️ mysql"],
+                "outcome":"This project revolutionizes how users interact with organizational websites, streamlining information access and enhancing user satisfaction. It represents a significant advancement in AI-driven solutions for improving online user experiences",
+                "demo":"chatbot_demo"},
+    
+    "proactive":{"description":"proactive description",
+                 "technologies":["🐍 python","🤖 python-telegram-bot","🐳 docker","🕵️‍♂️ Django","📊 matplotlib"],
+                 "outcome":"it removed burden from system admins",
+                 "demo":"proactive_demo"}}
 
 about_section = """👋 Hello! I'm Lemi Melkamu, a software developer with a deep interest in machine learning. I'm constantly motivated by the opportunity to learn and grow in the ever-evolving tech landscape.
 💻 My journey in the tech industry has allowed me to explore various domains, including software development, machine learning, chatbots, computer vision, and mobile applications using Flutter. Additionally, I've been exposed to areas like networking and system administration, although I wouldn't claim expertise or extensive experience in these fields just yet.
 
-🌟 I'm grateful for every opportunity to collabSorate with talented individuals and contribute to meaningful projects. Proficient in Afan Oromo, English, and Amharic, I strive to foster strong connections and effective collaboration within diverse teams."""
+🌟 I'm grateful for every opportunity to collaborate with talented individuals and contribute to meaningful projects. Proficient in Afan Oromo, English, and Amharic, I strive to foster strong connections and effective collaboration within diverse teams."""
 
 
 
-demos = {"jit_library_demo":"jit_library_demo","chatbot_demo":"D:\PERSONAL\MYPC\PROJECTS\python\IMPORTANT\CHATBOTS\my_portfolio_bot\chatbot_demo.mp4","proactive_demo":"proactive_demo"}
+demos = {"jit_library_demo":"D:\PERSONAL\MYPC\PROJECTS\python\IMPORTANT\CHATBOTS\my_portfolio_bot\jit_library_demo.mp4","chatbot_demo":"","proactive_demo":"proactive_demo"}
 
+PROJECT_NAMES = {
+    "jit_library":"JITLibTracker",
+    "chat_bot":"Chatbot System for Organization Websites",
+    "proactive":"Proactive Chatbot System"}
+    
 
 
 tech_urls = {
-    "python":"https://www.python.org/",
-    "opencv":"https://opencv.org/",
-    "pyqt5":"https://www.riverbankcomputing.com/software/pyqt/intro",
-    "mysql":"https://www.mysql.com/",
-    "SVM":"https://scikit-learn.org/stable/modules/svm.html",
-    "React":"https://reactjs.org/",
-    "Flask":"https://flask.palletsprojects.com/en/2.2.x/",
-    "docker":"https://www.docker.com/",
-    "Django":"https://www.djangoproject.com/",
-    "matplotlib":"https://matplotlib.org/"
+    " python":"https://www.python.org/",
+    " opencv":"https://opencv.org/",
+    " pyqt5":"https://www.riverbankcomputing.com/software/pyqt/intro",
+    " mysql":"https://www.mysql.com/",
+    " SVM":"https://scikit-learn.org/stable/modules/svm.html",
+    " React":"https://reactjs.org/",
+    " Flask":"https://flask.palletsprojects.com/en/2.2.x/",
+    " docker":"https://www.docker.com/",
+    " Django":"https://www.djangoproject.com/",
+    " python-telegram-bot":"https://docs.python-telegram-bot.org/en/v21.4/",
+    " matplotlib":"https://matplotlib.org/"
         }
+
+# import emoji
+# def clean_emoji(text)->str: 
+#         cleaned_text = emoji.replace_emoji(text, replace=" ")
+#         return cleaned_text
+# tech = [tech for tech in ProjectInfo["chat_bot"]['technologies'] ]
+# # print(tech_urls[clean_emoji(tech)
+# if __name__ == "__main__":
+#     print('#### Program starts Here')
+#     for tech in tech:
+#         print(tech_urls[clean_emoji(tech)])
+    
--- tg_portfolio.py
+++ tg_portfolio.py
@@ -1,7 +1,7 @@
 import logging
 from telegram.ext import *
 import re,emoji,time
-from config import bot_token,my_chat_id,ProjectInfo,about_section,demos, tech_urls
+from config import bot_token,my_chat_id,ProjectInfo,about_section,demos, tech_urls,PROJECT_NAMES
 from telegram import InlineKeyboardButton , InlineKeyboardMarkup, Update , callbackquery
 
 #swx@2WS
@@ -37,83 +37,79 @@ def project_details(update:Update, context:CallbackContext) -> None:
         [
             InlineKeyboardButton("📈 Outcome", callback_data='outcome'),
             InlineKeyboardButton("💡 Code Snippets/Demos", callback_data='demo'),
-        ],
-        [InlineKeyboardButton("⬅️ Back", callback_data="back")]
+        ]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-    query.edit_message_text('Select an option to learn more about the project:', reply_markup=reply_markup)
-   
+    # query.edit_message_text('Select an option to learn more about the project:', reply_markup=reply_markup)
+    context.bot.send_message(chat_id=query.message.chat_id,text='Here are some details for the {} project:'.format(project), reply_markup=reply_markup)
+
 def my_handler(update: Update,context:CallbackContext) -> None: # type: ignore
     # swx@2WS 
     query = update.callback_query
     query.answer()
     data = query.data
     global stack
-    stack.append(query.data)
-    print(f'Query data : {data}   is selected')
-    print(f'and Here is the stack content: {stack}')
     if data == 'yes':
-        keyboard = [
-        [
-            InlineKeyboardButton("👤 About Me", callback_data='about_me'),
-            InlineKeyboardButton("📁 Projects", callback_data='projects'),
-        ],
-        [
-            InlineKeyboardButton("💼 Skills", callback_data='skills'),
-            InlineKeyboardButton("✉️ Contact Me", callback_data='contact_me'),
-        ],
-        [InlineKeyboardButton("⬅️ Back", callback_data="read_more")]
-    ]
-
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        query.edit_message_text('choose the options please',reply_markup=reply_markup)
-    if data in ['read_more','about_me','projects','skills','contact_me','jit_library','chat_bot','proactive','project_description','technologies_used','outcome','demo']:
-        inner_handler(update,context,data)
-    if data == 'back':
-        global count 
-        count = count + 1
-        if count >= 1:
-            data=stack[-2]
-        print(f'you are passing {data} to the inner_handler method.')
-        inner_handler(update,context,data)
-    if query.data == 'no':
-        query.edit_message_text('OK please check out when you feel like to, anytime i\'m available thanks.')   
+            stack.append(query.data)
+            keyboard = [
+            [
+                InlineKeyboardButton("👤 About Me", callback_data='about_me'),
+                InlineKeyboardButton("📁 Projects", callback_data='projects'),
+            ],
+            [
+                InlineKeyboardButton("💼 Skills", callback_data='skills'),
+                InlineKeyboardButton("✉️ Contact Me", callback_data='contact_me'),
+            ]
+        ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            query.edit_message_text('Get to know me better',reply_markup=reply_markup)
+            
+    if stack and data != stack[-1]:
+        stack.append(query.data)
+        print(f'Query data : {data}   is selected')
+        print(f'and Here is the stack content: {stack}')
+        if data in ['read_more','about_me','projects','skills','contact_me','jit_library','chat_bot','proactive','project_description','technologies_used','outcome','demo']:
+            inner_handler(update,context,data)
+        if query.data == 'no':
+            query.edit_message_text('OK please check out when you feel like to, anytime i\'m available thanks.')   
 def inner_handler(update:Update,context:CallbackContext,data)->None:
     query = update.callback_query
     print(f"we are in inner_handler method and stack content is : {stack}")
     if data == 'projects':
-        menus = [[InlineKeyboardButton('JIT Library monitoring',callback_data='jit_library')],[InlineKeyboardButton('Chatbot system for organizations website',callback_data='chat_bot')],[InlineKeyboardButton('Proactive monitoring',callback_data='proactive')],
-                 [InlineKeyboardButton("⬅️ Back", callback_data="back")]]
+        menus = [[InlineKeyboardButton('JITLibTracker',callback_data='jit_library')],[InlineKeyboardButton('Chatbot system for organizations website',callback_data='chat_bot')],[InlineKeyboardButton('Proactive monitoring',callback_data='proactive')]]
+        # menus.append([InlineKeyboardButton("⬅️ Back", callback_data="back")])
         reply_markup = InlineKeyboardMarkup(menus)
-        query.edit_message_text('Here are some of my projects',reply_markup=reply_markup)
-    
+        # query.edit_message_text('Here are some of my projects',reply_markup=reply_markup)
+        context.bot.send_message(chat_id=query.message.chat_id,text="Here are some of my projects",reply_markup=reply_markup)
+
     if data in ['jit_library','chat_bot','proactive']:
         project_details(update,context)       
     if data in ['project_description','technologies_used','outcome','demo']:
         handle_project_details(update,context,data)
     if data == "about_me":
         text = about_section
-        button = [[InlineKeyboardButton("⬅️ Back", callback_data="back")]]
-        query.edit_message_text(text=text)
-        query.message.reply_markup(reply_markup=InlineKeyboardMarkup(button))
-        
+        context.bot.send_message(chat_id=query.message.chat_id,text=text)    
+            
     if data == "contact_me":
         my_github = "https://github.com/lemidb"
-        my_linkedin = "https://www.linkedin.com/in/lemidbgelnemerd2"
+        my_linkedin = "https://www.linkedin.com/in/lemidbgelnemerd23/"
         my_twitter = "http://x.com/lemi_melkamu"
         my_telegram = "https://t.me/@lemidb"
         
-        contact_keyboard = [[InlineKeyboardButton("💼 LinkedIn", url = my_linkedin),InlineKeyboardButton("🐙 GitHub", url = my_github)],[InlineKeyboardButton("🐦 Twitter",url=my_twitter),InlineKeyboardButton("🛸 Telegram",url=my_telegram)],[InlineKeyboardButton("⬅️ Back", callback_data="back")]]
-        reply_markup = InlineKeyboardMarkup(contact_keyboard)
-        query.edit_message_text(text='Here are my contact details:',reply_markup=reply_markup)
+        contact_keyboard = [[InlineKeyboardButton("💼 LinkedIn", url = my_linkedin),InlineKeyboardButton("🐙 GitHub", url = my_github)],[InlineKeyboardButton("🐦 Twitter",url=my_twitter),InlineKeyboardButton("🛸 Telegram",url=my_telegram)]]
+        # reply_markup = InlineKeyboardMarkup(contact_keyboard)
+        # query.edit_message_text(text='Here are my contact details:',reply_markup=reply_markup)
+        new_keyboard = InlineKeyboardMarkup(contact_keyboard)
+        context.bot.send_message(chat_id=query.message.chat_id, text="My contacts: ",
+                                 reply_markup=new_keyboard)
        
 
 def print_and_return(value):
-    print(f'## This tech values after filtered. {value}')
+    print(f'@@!!&&&&## This tech values after filtered. {value}')
     return value
 
-def clean_emoji(text):
+def clean_emoji(text): 
         cleaned_text = emoji.replace_emoji(text, replace="")
         return cleaned_text
     
@@ -123,40 +119,41 @@ def handle_project_details(update: Update,context:CallbackContext,data) -> None:
     print('## Stack status: ',stack)
     global project
     if stack:
-        project = stack[-2]
-    print(f"Here is the last selected project: {project}")
+        for pro in reversed(stack):
+            if pro in ProjectInfo.keys():
+                project = pro
+                break
+    print(f"Here is the last selected project: {PROJECT_NAMES[project]}")
     if project in ProjectInfo.keys():
-        print(f'Oohhh! Great {project} is in ProjectInfo dictionary ')
+        print(f'Oohhh! Great {PROJECT_NAMES[project]} is in ProjectInfo dictionary ')
         if data == 'project_description':
-            
             if project in ProjectInfo.keys():
                 description=ProjectInfo[project]['description']
-                buttons = [[InlineKeyboardButton("⬅️ Back", callback_data="back")]]
-                reply_markup = InlineKeyboardMarkup(buttons)
-                query.edit_message_text(text="DESCRIPTION for the project: {} :".format(project),reply_markup=reply_markup)
+                # query.edit_message_text(text="DESCRIPTION for the project: {} :".format(project),reply_markup=reply_markup)
+                context.bot.send_message(chat_id=query.message.chat_id,text=description)
+                
         if data =="technologies_used":
             technologies=ProjectInfo[project]['technologies']
-            # for tech in technologies:
-            #     cleaned_tech = clean_emoji(tech)
-            print(f'@#@#@ Processing data for project: {project}  technologies: {technologies}')
-            buttons = [[InlineKeyboardButton(tech, url=print_and_return(tech_urls[clean_emoji(tech)]))] for tech in technologies]
-            buttons.append([InlineKeyboardButton("⬅️ Back", callback_data="back")])
+            print(f'@#@#@ Processing data for project: {PROJECT_NAMES[project]}  technologies: {technologies}')
+            buttons = [[InlineKeyboardButton(tech, url = tech_urls[clean_emoji(tech)])] for tech in technologies]
+            # buttons.append([InlineKeyboardButton("⬅️ Back", callback_data="back")])
             reply_markup = InlineKeyboardMarkup(buttons)
-            query.edit_message_text(text="Here are the technologies used for {}:".format(project),reply_markup=reply_markup)
-        
+            # query.edit_message_text(text="Here are the technologies used for {}:".format(project),reply_markup=reply_markup)
+            context.bot.send_message(chat_id=query.message.chat_id,text="Here are the technologies used for {}:".format(PROJECT_NAMES[project]),reply_markup=reply_markup)
         if data == "outcome":
             outcome=ProjectInfo[project]['outcome']
-            buttons = [[InlineKeyboardButton(outcome,callback_data='outcome')],[InlineKeyboardButton("⬅️ Back", callback_data="back")]]
-            reply_markup = InlineKeyboardMarkup(buttons)
-            query.edit_message_text(text="Outcome of the project {}: ".format(project),reply_markup=reply_markup)
+            # buttons = [[InlineKeyboardButton(outcome,callback_data='outcome')]]
+            # reply_markup = InlineKeyboardMarkup(buttons)
+            # query.edit_message_text(text="Outcome of the project {}: ".format(project),reply_markup=reply_markup)
+            context.bot.send_message(chat_id=query.message.chat_id,text=outcome)
         if data == "demo":
             demo=ProjectInfo[project]['demo']
             demo_path = demos[demo]
             print(f"Here is the demo file: {demo_path}")
-            if project == "chat_bot":
-               context.bot.send_message(chat_id=query.message.chat_id, text="please wait while the video loads")   
+            if project == "jit_library":
+                context.bot.send_message(chat_id=query.message.chat_id, text="please wait while the video loads")   
             with open(f"{demo_path}",'rb') as video:
-                context.bot.send_video(chat_id=query.message.chat_id, video=video, caption="Here's my demo for the {} project. Enjoy!".format(project))
+                context.bot.send_video(chat_id=query.message.chat_id, video=video, caption="Here's my demo for the {} project. Enjoy!".format(PROJECT_NAMES[project]))
         
 def error_handler(update: Update, context: CallbackContext) -> None:
     """Log the error and send a telegram message to notify the developer."""
